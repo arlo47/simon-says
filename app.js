4a028178e1ba3a6cf332df9e2db716e7612cf278
gfitzpatrick2 <gfitzpatrick2@gmail.com>
2018-01-20 14:47:54 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -2,25 +2,40 @@
 // blue = 2
 // green = 3
 // yellow = 4
-
 let red = document.getElementById('red');
 let blue = document.getElementById('blue');
 let green = document.getElementById('green');
 let yellow = document.getElementById('yellow');
 
+//audio variables
+let redAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound1.mp3');
+let blueAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound2.mp3');
+let greenAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound3.mp3');
+let yellowAudio = new Audio('https://s3.amazonaws.com/freecodecamp/simonSound4.mp3');
+
+//node list containing all the above colors
+let colorNodeList = document.querySelectorAll('.circle');
+
+//This is for outputting into the DOM
+let says = document.getElementById('says');
+
 //simonSequence is the randomly generated array of colors
 let simonSequence = [];
+
 //userSequence is the array the user generates by clicking on the colors to compare to simonSequence
 let userSequence = [];
+
+//Counter of how many rounds played
+let roundCount = 1;
 
 let game = {
   addToSequence: function() {
     //This Math generates a random number between 1 and 4 which is then pushed to simonSequence in the switch below
-    let lastInSequence = Math.floor(Math.random() * 4) + 1;
+    let newColor = Math.floor(Math.random() * 4) + 1;
     //resets userSequence for the next round
-    userSequence.length = 0;
+    userSequence.splice(0);
 
-    switch (lastInSequence) {
+    switch (newColor) {
       case 1:
         simonSequence.push('red');
         console.log('red');
@@ -40,50 +55,105 @@ let game = {
       default:
           console.log('game.addToSequence() error');
     }
+    roundCount++;
+    effects.doEffects();
   },
   userResponse: function(e) {
     //e.target.id returns the html id as a string, .push() adds it to userSequence
     userSequence.push(e.target.id);
+    
+    switch (e.target.id) {
+      case 'red':
+        redAudio.play();
+        break;
+      case 'blue':
+        blueAudio.play();
+        break;
+      case 'green':
+      greenAudio.play();
+        break;
+      case 'yellow':
+      yellowAudio.play();
+        break;
+      default:
+          console.log('game.addToSequence() error');
+    }
+
+    if (userSequence.length === simonSequence.length) {
+      game.compareSequences();
+    }
   },
   compareSequences: function() {
-    //Checks if the two sequences are equal in length before doing any other tests
-    if (simonSequence.length === userSequence.length) {
-        for (let i = 0; i < simonSequence.length; i++) {
-          //iterates over both sequences and returns false if they are not exactly the same
-          if (userSequence[i] !== simonSequence[i]) {
-            console.log('User guessed wrong.');
-            return false;
-          }
+      for (let i = 0; i < simonSequence.length; i++) {
+        //iterates over both sequences and returns false if they are not exactly the same
+        if (userSequence[i] !== simonSequence[i]) {
+          console.log('User guessed wrong.');
+          says.textContent = 'You guessed wrong. Play again?';
+          return false;
+        }
       }
       console.log('Correct!');
+      says.textContent = `Correct! Starting round ${roundCount}.`;
+      
       //If the two sequences are exactly the same, run addToSequence() again for the next round.
       game.addToSequence();
-    } else {
-      console.log('Simon and the user\'s sequence aren\'t the same length');
-    }
   }
 };
 
-
-function sequenceEffects() {
-  let i = 0;
-
-  if (red.classList.contains('opacity')) {
-    red.classList.remove('opacity');
-  }
-
-  setTimeout(function() {
-    red.classList.add('opacity');
-    i++;
-    if (i < 1) {
-      sequenceEffects();
+let effects = {
+  doEffects: function() {
+    let i = 0;
+  
+    let interval = setInterval(function() {
+      effects.addRemoveEffect(i);
+  
+      i++;
+      if (i >= simonSequence.length) {
+        clearInterval(interval);
+      }
+    }, 800);
+  },
+  addRemoveEffect: function(i) {
+    switch (simonSequence[i]) {
+      case 'red':
+        colorNodeList[0].classList.add('opacity');
+        redAudio.play();
+        break;
+      case 'blue':
+        colorNodeList[1].classList.add('opacity');
+        blueAudio.play();
+        break;
+      case 'green':
+        colorNodeList[2].classList.add('opacity');
+        greenAudio.play();
+        break;
+      case 'yellow':
+        colorNodeList[3].classList.add('opacity');
+        yellowAudio.play();
+        break;
+      default:
+        console.log('effects.addRemoveEffect() error (adding)');
     }
-  }, 1000);
-}
-
-
-
-
+    setTimeout(function() {
+      switch (simonSequence[i]) {
+        case 'red':
+          colorNodeList[0].classList.remove('opacity');
+          break;
+        case 'blue':
+          colorNodeList[1].classList.remove('opacity');
+          break;
+        case 'green':
+          colorNodeList[2].classList.remove('opacity');
+          break;
+        case 'yellow':
+          colorNodeList[3].classList.remove('opacity');
+          break;
+        default:
+          console.log('effects.addRemoveEffect() error (removing)');
+      }
+    }, 400);
+  }
+};
 
 red.addEventListener('click', game.userResponse);
 blue.addEventListener('click', game.userResponse);
